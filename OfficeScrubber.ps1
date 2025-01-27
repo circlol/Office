869588892689a9ca481943e684f24e416520c936
circlol <65281163+circlol@users.noreply.github.com>
2025-01-27 03:33:08 -0800

    Update OfficeScrubber.ps1

--- OfficeScrubber.ps1
+++ OfficeScrubber.ps1
@@ -6,8 +6,8 @@ $ErrorActionPreference = [System.Management.Automation.ActionPreference]::Silent
 function Get-Question {
     $answer = Read-Host -Prompt "Would you like to run SaRA? (Y/N)"
     switch ($answer.ToUpper()) {
-        'Y' { Remove-Office }
-        'N' { return }
+        'Y' { return $True }
+        'N' { return $False}
         Default { Get-Question }
     }
 }
@@ -33,29 +33,33 @@ function Remove-Office {
     
    Office Removal Tool
 
-   Office Products Installed:
-   $InstalledOffice
-   
-   What will happen?`n   The latest version of the Microsoft Support and Recovery Assistant (SaRA) tool will be downloaded and run using the commands below:
-   
-   SaRACMD.exe -OfficeScrubScenario -AcceptEula -OfficeVersion All 
-   
-   URL: https://aka.ms/SaRA_EnterpriseVersionFiles
-   THIS TOOL WILL REMOVE EVERY VERSION OF OFFICE.`n        THIS IS THE ONLY CONFIRMATION`n`n"
-    Get-Question
+Office Products Installed:
+$InstalledOffice
 
-    # Downloads
-    try { Write-Output "Downloading SaRA."
-        Invoke-WebRequest https://aka.ms/SaRA_EnterpriseVersionFiles -OutFile $env:temp
+What will happen?`nThe latest version of the Microsoft Support and Recovery Assistant (SaRA) tool will be downloaded and run using the commands below:
+
+SaRACMD.exe -OfficeScrubScenario -AcceptEula -OfficeVersion All 
+
+URL: https://aka.ms/SaRA_EnterpriseVersionFiles
+THIS TOOL WILL REMOVE EVERY VERSION OF OFFICE.`n     THIS IS THE ONLY CONFIRMATION`n`n"
+
+    $answer = Get-Question
+
+
+
+    If ($answer -eq $True) {
+        # Downloads
+        try { Write-Output "Downloading SaRA."
+        Invoke-WebRequest https://aka.ms/SaRA_EnterpriseVersionFiles -OutFile $env:temp 
     }
     catch { return "Error downloading SaRA:`n$($Error[0])" }
-
+    
     # Extracts
     try { Write-Output "Extracting files."
         Expand-Archive -Path "$env:temp\SaRACmd_*.zip" -DestinationPath "$env:temp\SaRA" -Force
     }
     catch { return "Error expanding SaRA:`n$($Error[0])" }
-    
+
     # Starts
     try { Write-Output "Starting SaRA."
         Start-Process (Get-ChildItem "$env:temp\SaRA" -Include SaRAcmd.exe -Recurse).FullName -ArgumentList "-S OfficeScrubScenario -AcceptEula -OfficeVersion All" -Wait -NoNewWindow
@@ -68,8 +72,11 @@ function Remove-Office {
         Remove-Item "$env:temp\SaRA" -Force
     }
     catch { return "Error Cleaning up:`n$($Error[0])" }
-} 
+    } else {
+        return "Closing."
+    }
 
+}
 
 
 
